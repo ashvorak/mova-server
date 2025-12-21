d332d5a508f2e9ba1e959be6e11d318358bbd949
Oleksii Shvorak <aleksey.shvorak@gmail.com>
2025-12-21 22:33:48 +0200

    Add health handler

--- internal/httpserver/server.go
+++ internal/httpserver/server.go
@@ -1,13 +1,20 @@
 package httpserver
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
+type HealthResponse struct {
+	Status  string `json:"status"`
+	Service string `json:"service"`
+}
+
 func New() *http.Server {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/ping", pingHandler)
+	mux.HandleFunc("/api/health", healthHandler)
 
 	s := &http.Server{
 		Addr:    ":8080",
@@ -18,5 +25,25 @@ func New() *http.Server {
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "pong")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("pong"))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	response := &HealthResponse{
+		Status:  "ok",
+		Service: "mova-server",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+	}
 }
